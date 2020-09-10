f7609015280d68b12098fce9895493f26a226f96
Elle <elleobrien11@gmail.com>
2020-09-10 14:09:28 -0700

    fixed # of inputs

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from sklearn.linear_model import Lasso
 import pickle
 import os
 
-X, y = make_regression(10000,n_features = 10)
+X, y = make_regression(10000,n_features = 11)
 
 # Train a model
 reg = Lasso().fit(X, y.ravel())

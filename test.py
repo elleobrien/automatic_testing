042b3a3df7eaa11b27018a98d0f77e67e6bf3845
Tammo Hesselink <t.hesselink@bluefield.eu>
2022-07-26 09:40:20 +0200

    Trained new model

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import json
 model = pickle.load(open("models/model.pkl", "rb"))
 
 # Generate some data for validation
-X_test, y = make_regression(1000,n_features = 11)
+X_test, y = make_regression(1000,n_features = 10)
 
 # Test on the model
 y_hat = model.predict(X_test)
